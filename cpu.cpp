66fdbe08b2158f0647977070239cc6bdf00c4b93
Irokanade <michaeljs.leong@gmail.com>
2026-03-31 09:46:14 +0800

    fix: added l2 dtlb lookup in translate

--- cpu.cpp
+++ cpu.cpp
@@ -4,21 +4,29 @@
 
 #include <cstdint>
 
-void translate(Core *core, Memory *mem, uint64_t virtual_address, uint64_t *physical_address) {
+bool translate(Core *core, Memory *mem, uint64_t virtual_address, uint64_t *physical_address) {
     uint64_t virtual_page_num = to_frame(virtual_address);
     uint64_t physical_frame;
 
     if (tlb_lookup(core->l1_dtlb, L1_DTLB_SETS, virtual_page_num, &physical_frame)) {
         *physical_address = to_address(physical_frame, virtual_address & 0xFFF);
-        return;
+        return true;
+    }
+
+    if (tlb_lookup(core->l2_dtlb, L2_DTLB_SETS, virtual_page_num, &physical_frame)) {
+        *physical_address = to_address(physical_frame, virtual_address & 0xFFF);
+        tlb_fill(core->l1_dtlb, L1_DTLB_SETS, virtual_page_num, physical_frame);
+        return true;
     }
 
     if (page_walk(mem, virtual_address, physical_address)) {
         uint64_t frame = to_frame(*physical_address);
         tlb_fill(core->l1_dtlb, L1_DTLB_SETS, virtual_page_num, frame);
         tlb_fill(core->l2_dtlb, L2_DTLB_SETS, virtual_page_num, frame);
-        return;
+        return true;
     }
+
+    return false; // page fault
 }
 
 bool cpu_read(Core *core, uint64_t addr, uint8_t *data) {
